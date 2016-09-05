4b92bfac279813fedb56b16140944fb6282e09dd
fxcosta <fx3costa@gmail.com>
2016-09-05 01:58:59 -0300

    modal sendo exibido onload e com validação de campos

--- src/app/twitter/twitter.component.ts
+++ src/app/twitter/twitter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ViewEncapsulation, Input, ViewChild, Renderer} from '@angular/core';
 
 import { NgSemanticModule } from "ng-semantic";
 
@@ -8,25 +8,34 @@ import { NgSemanticModule } from "ng-semantic";
   styleUrls: ['./twitter.component.css']
 })
 
-export class TwitterComponent implements OnInit {
+export class TwitterComponent implements OnInit, AfterViewInit {
 
   public tweets: Object[] = [];
-  public name: string;
+  public nameUser: string = '';
+  public messageModal: string = '';
 
-  public hideElement: boolean;
+  @ViewChild('myModalNormal') myModalNormal;
 
-
-  constructor() {
-
-  }
+  constructor(private renderer: Renderer) { }
 
   onTweet(tweet: HTMLInputElement) {
     this.tweets.push({ value: tweet.value, date: new Date()});
     tweet.value = null;
   }
 
-  ngOnInit() {
-    this.hideElement = true;
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.renderer.invokeElementMethod(this.myModalNormal, 'show');
+  }
+
+  saveNameAndCloseModal(nameUser: string) {
+    if (!nameUser) {
+      this.messageModal = "Por favor, é necessário um nome de usuário. Pode ser qualquer coisa!";
+    } else {
+      this.nameUser = nameUser;
+      this.renderer.invokeElementMethod(this.myModalNormal, 'hide');
+    }
   }
 
 }
